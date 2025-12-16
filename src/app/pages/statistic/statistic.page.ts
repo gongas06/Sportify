4b11a753ed3554ef20d3631105ff4a8f3c0beb44
Gongas06 <goncaloferreira@pvc.pt>
2025-12-16 11:34:55 +0000

    Atualização da statistic

--- src/app/pages/statistic/statistic.page.ts
+++ src/app/pages/statistic/statistic.page.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-statistic',
   templateUrl: './statistic.page.html',
   styleUrls: ['./statistic.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule],
 })
-export class StatisticPage implements OnInit {
+export class StatisticPage {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
